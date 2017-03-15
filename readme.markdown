4943b278c372d361be1d34f4ec8cccfe5d930221
lesliepeng <xiaomeng1207@gmail.com>
2017-03-15 15:41:40 +0800

    initial commit

---
+++ readme.markdown
@@ -0,0 +1,15 @@
+## My blog host on github
+
+## [fraserxu.me](https://fraserxu.me)
+
+Install dependencies
+
+```
+$ bundle
+```
+
+Start the server
+
+```
+$ bundle exec jekyll serve
+```
---
+++ sw.js
@@ -0,0 +1,55 @@
+---
+
+---
+
+'use strict';
+
+importScripts('public/js/serviceworker-cache-polyfill.js')
+
+const CACHE_VERSION = '{{ site.time }}'
+const staticCacheName = 'static-v' + CACHE_VERSION
+
+self.addEventListener('install', function (event) {
+  event.waitUntil(
+    caches.open(staticCacheName).then(function(cache) {
+      return cache.addAll([
+        '/public/css/lanyon.css',
+        '/public/css/poole.css',
+        '/public/css/syntax.css',
+        '/public/js/index.js',
+        '/public/js/serviceworker-cache-polyfill.js',
+        '//fonts.lug.ustc.edu.cn/css?family=PT+Serif:400,400italic,700%7CPT+Sans:400',
+      ])
+    })
+  )
+})
+
+self.addEventListener('activate', function (event) {
+  event.waitUntil(
+    caches.keys().then(function (cacheNames) {
+      return Promise.all(
+        cacheNames.filter(function (cacheName) {
+          return cacheName.startsWith('static-v') &&
+                 cacheName !== staticCacheName
+        }).map(function (cacheName) {
+          return caches.delete(cacheName)
+        })
+      )
+    })
+  )
+})
+
+self.addEventListener('fetch', function (event) {
+  event.respondWith(
+    caches.match(event.request).then(function (response) {
+      return response || fetch(event.request)
+    })
+  )
+})
+
+self.addEventListener('message', function (event) {
+  if (event.data.action === 'skipWaiting') {
+    console.log('skipWaiting...')
+    self.skipWaiting()
+  }
+})
